db1904284fe4449f4260accb5e2702354ece2017
saiyogesh6599 <saiyogesh@MacBookAir.home>
2024-10-15 19:49:46 -0500

    Project2

---
+++ Hibernate/src/test/java/Project2/Hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package Project2.Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
