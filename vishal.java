89d318094147e255767596e88c3c26a603e95279
ksaiyogesh1704@gmail.com <saiyogesh@MacBookAir.home>
2025-01-29 22:46:00 -0600

    new changes

---
+++ vishal.java
@@ -0,0 +1,55 @@
+//package vishalP;
+//
+//import java.util.Scanner;
+//
+//public class vishal {
+//	
+//		//public static void main (String args[]) {
+// 
+//			Scanner sc=new Scanner(System.in);
+//			System.out.println("Enter the number input");
+//		
+//			int number=sc.nextInt();
+//			String digit= ""+number;
+//			char[] digits1=digit.toCharArray();
+//			int s=0;
+//			
+//			for (int i=0;i<digits1.length;i++) {
+//				 int num=Integer.parseInt(digits1[i]+"");
+//				//System.out.println(digits1[i]);
+//				 s=s+num;
+//
+//			}
+//			System.out.println(s);
+//		}
+//}
+
+List<acountNew> list=new ArrayList()<>;
+
+public main() {
+	while(n!=0){
+	if (1) {
+		accountNew a=new ame();
+		syout(enter name);
+		a.set(sc.nextInt());
+		sou(ent ac);
+		list.add(a);
+	}
+	else {
+		int acoutn=sc.nex;
+		booean bool=dslse;
+		for(Account n:list) {
+			if(n.getaccount().equl(aoubt)) {
+				sys(n);
+				bool=true;
+			}
+		}
+		if(bolea) {
+			syst();
+		}
+	}
+	syet("Do you want to con p1");
+	}
+}
+
+
